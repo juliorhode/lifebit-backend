aa960988b02f23d941ca893347c8b6a5acf6e612
Julio Rhode <juliorhode@gmail.com>
2025-07-03 02:45:38 -0400

    chore: Initial project setup and configuration

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
 const { Pool, Query } = require('pg')
 
 const pool = new Pool({
+	// Cada una de estas propiedades lee su valor directamente de 'process.env'
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
 	database: process.env.DB_DATABASE,
@@ -8,6 +9,8 @@ const pool = new Pool({
 	port: process.env.DB_PORT,
 })
 
+// Exportamos un objeto que contiene una única función llamada 'query'.
+// En lugar de importar y usar el 'pool' en todas partes, solo importaremos este archivo y llamaremos a db.query().
 module.exports = {
 	query: (text, params) => pool.query(text, params),
 }
